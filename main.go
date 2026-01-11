d2efa1344aa12944323274687dd3ff3a91de3b5a
cooperbraun13 <cooperjbraun13@gmail.com>
2026-01-11 12:26:28 -0800

    feat: implemented the Update method to deal with different type of messages from the http response

--- main.go
+++ main.go
@@ -39,3 +39,45 @@ type errMsg struct{ err error }
 func (e errMsg) Error() string {
 	return e.err.Error()
 }
+
+func (m model) Init() tea.Cmd {
+	return checkServer
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case statusMsg:
+		// the server returned a status message. save it to our model. also
+		// tell the Bubble Tea runtime we want to exit because we have nothing
+		// else to do. we'll still be able to render a final view with our
+		// status message
+		m.status = int(msg)
+		return m, tea.Quit
+
+	case errMsg:
+		// there was an error. note it in the model. and tell the runtime
+		// we're done and want to quit
+		m.err = msg
+		return m, tea.Quit
+
+	case tea.KeyMsg:
+		// Ctrl+c exists. even with short running programs it's good to have
+		// a quit key, just in case your logic is off. users will be very
+		// annoyed if they can't exit
+		if msg.Type == tea.KeyCtrlC {
+			return m, tea.Quit
+		}
+	}
+
+	// if we happen to get any other messages, don't do anything
+	return m, nil
+}
+
+func (m model) View() string {
+
+}
+
+func main() {
+
+}
